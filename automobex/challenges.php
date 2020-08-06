919bc0026aac6bc8a265568e72564bb56dc32a46
Jorden Hodges <58154433+jLewisHodges@users.noreply.github.com>
2020-08-06 09:31:45 -0400

    Add files via upload

---
+++ automobex/challenges.php
@@ -0,0 +1,6 @@
+<?php 
+$the_title = 'Challenge Center';
+include('header.php');?>
+    <!--HTML Code Here-->
+<?php
+include('footer.php');?>
---
+++ automobex/education.php
@@ -0,0 +1,6 @@
+<?php 
+$the_title = 'Education Center';
+include('header.php');?>
+    <!--HTML Code Here-->
+<?php
+include('footer.php');?>
